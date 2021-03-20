783fedc42e987682d03e37666d042a86b5191325
russel0014 <russellobo0014@gmail.com>
2021-03-20 21:05:12 +0530

    fourth commit

--- job portal/jobportal/DataFlairJobPortal/JOBS/asgi.py
+++ job portal/jobportal/DataFlairJobPortal/JOBS/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DataFlairJobPortal.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'JOBS.settings')
 
 application = get_asgi_application()
--- job portal/jobportal/DataFlairJobPortal/JOBS/settings.py
+++ job portal/jobportal/DataFlairJobPortal/JOBS/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'DataFlairJobPortal.urls'
+ROOT_URLCONF = 'JOBS.urls'
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'DataFlairJobPortal.wsgi.application'
+WSGI_APPLICATION = 'JOBS.wsgi.application'
 
 
 # Database

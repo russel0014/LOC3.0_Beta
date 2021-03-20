f80f60f17f0a10b092d6953c7956063700f883ef
russel0014 <russellobo0014@gmail.com>
2021-03-20 15:55:44 +0530

    first commit

---
+++ job portal/jobportal/job/views.py
@@ -0,0 +1,49 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User,auth
+from django.contrib import messages
+
+
+
+def index(request):
+    return render(request,"index.html",)
+
+
+def signin(request):
+    if request.method=='POST':
+        first_name=request.POST['first_name']
+        last_name=request.POST['last_name']
+        user_name=request.POST['user_name']
+        email=request.POST['email']
+        password=request.POST['password']
+        validpassword=request.POST['validpassword']
+        if password==validpassword:
+            if User.objects.filter(username=user_name).exists():
+                messages.info(request,"username taken")
+                return redirect('index')
+            elif User.objects.filter(email=email).exists():
+                messages.info(request,"email taken")
+                return redirect('index')
+            else:
+                user=User.objects.create_user(username=user_name,password=password,first_name=first_name,email=email,last_name=last_name)
+                user.save()
+        else:
+            messages.info(request,"username taken")
+            return redirect('index')
+        return redirect('/') 
+    else:    
+        return render(request,"signin.html")
+
+    def login(request) :
+        if request.method=='POST' :
+            username=request.POST['username']
+            password=request.POST['password']
+            user=auth.authenticate(username=username,password='password')
+            if user is not None:
+                auth.login(request,user)
+                return redirect("/")
+            else:
+                messages.info(request,'login')
+        else :
+            return render(request,'login.html')
+
+
